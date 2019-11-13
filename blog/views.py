da02b2bab2cc52dd7996a79e4a5a9921f6fa997a
Francisca <franciscacruzat23@gmail.com>
2019-11-13 17:13:58 -0300

    Vistas de post_detail creadas y linkeadas a cada post según su id

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.db import models
 from django.utils import timezone
@@ -12,3 +12,8 @@ import datetime
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail (request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
